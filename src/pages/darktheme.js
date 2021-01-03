bfcbb9d83f95377f56257025846519119372a827
Gopalakrishnan <krishnan.gopal34@gmail.com>
2021-01-03 19:38:17 +0530

    dark

--- src/pages/darktheme.js
+++ src/pages/darktheme.js
@@ -1,17 +1,17 @@
 import * as React from "react"
-import About from "../components/About"
-import Banner from "../components/Banner"
-import Founder from "../components/Founder"
+// import About from "../components/About"
+// import Banner from "../components/Banner"
+// import Founder from "../components/Founder"
 // import Navbar from "../components/Navbar"
-import Team from '../components/Team'
-import { Sliderdata } from "../data/SliderData"
-import { GalleryData } from "../data/GalleryData"
-import { TeamData } from "../data/TeamData"
+// import Team from '../components/Team'
+// import { Sliderdata } from "../data/SliderData"
+// import { GalleryData } from "../data/GalleryData"
+// import { TeamData } from "../data/TeamData"
 import '../styles/main.scss'
-import Gallery from "../components/Gallery"
-import Contact from "../components/Contact"
-import Footer from "../components/Footer"
-import Event from "../components/Event"
+// import Gallery from "../components/Gallery"
+// import Contact from "../components/Contact"
+// import Footer from "../components/Footer"
+// import Event from "../components/Event"
 
 // markup
 const DarkTheme = () => {
@@ -20,14 +20,14 @@ const DarkTheme = () => {
     <main>
       <title>Home Page</title>
       {/* <Navbar /> */}
-      <Banner slides={Sliderdata} />
+      {/* <Banner slides={Sliderdata} />
       <About dark={true} />
       <Founder dark={true} />
       <Team teams={TeamData} dark={true} />
       <Gallery gallery={GalleryData} dark={true} />
       <Event dark={true} />
       <Contact dark={true} />
-      <Footer />
+      <Footer /> */}
     </main>
   )
 }
